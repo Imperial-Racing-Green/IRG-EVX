3fa6f85ac4e649529eb15ff2eef75a778e943ba8
Owen Heaney <oheaney380@gmail.com>
2018-11-23 16:15:45 +0000

    Camber and toe working
    Calculations now working for camber and toe - just need to get quat2angle() working in Simulink

---
+++ quaternion_test.m
@@ -0,0 +1,16 @@
+sze = size(quaternion_out.Data);
+camber = zeros(sze(1),1);
+toe = zeros(sze(1),1);
+rot = zeros(sze(1),1);
+
+for i = 1:sze(1)
+    [camber(i), toe(i), rot(i)] = quat2angle(quaternion_out.Data(i,:),'XZY');
+end
+camber = camber.*57.2958;
+toe = toe.*57.2958;
+plot(quaternion_out.Time,camber)
+hold on
+plot(quaternion_out.Time,toe)
+plot(quaternion_out.Time,rot)
+hold off
+legend('camber','toe','rotation','location','northwest')
