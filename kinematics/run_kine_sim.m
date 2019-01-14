928d8e21a9946fd08fc53607899882a8afe12af5
Owen Heaney <oheaney380@gmail.com>
2019-01-14 15:06:33 +0000

    Metrics Progress
    Most of the metrics are now calculated - next step is roll and steer properties

--- kinematics/run_kine_sim.m
+++ kinematics/run_kine_sim.m
@@ -10,10 +10,19 @@ disp("Parameterising model...")
 params = parameterise_kine_model(hardpoints_front);
 
 disp("Running simulation...")
+kine_model = load_system('Kinematics_Model');
 simOut = struct();
+
+%Make the visualisation window only appear if requested
+%TODO: figure this out...
+simOut.config = getActiveConfigSet('Kinematics_Model'); %Useful to save settings either way
+simScapeMB = getComponent(getComponent(simOut.config,'Simscape'),'SimscapeMultibody');
+
+%And run the simulation!
 simOut.SimulationOutput = sim('Kinematics_Model','SaveState','on','StateSaveName','xout',...
             'SaveOutput','on','OutputSaveName','yout',...
  'SaveFormat', 'Dataset', 'CaptureErrors', 'on');
+
 disp("Simulation complete")
 
 %% Tidy up simulation output variables
@@ -52,8 +61,9 @@ end
 %Sim runs two tests - helpful to take them separately
 
 n_data = length(simOut.channels.time);
-dl = diff(channels.l_damper);
-steer_test_end = find(dl~=0,1);
+dl = diff(simOut.channels.l_damper);
+da = diff(simOut.channels.a_toe);
+steer_test_end = find(round(da,1)~=round(da(1),1),1) + 1;
 vert_test_start = find(round(dl,2)~=round(dl(end),2),1,'last');
 
 %% Calculate Dynamic Instant Centre Position
@@ -69,7 +79,6 @@ lwb_out = [position_output.lwb_x.data, position_output.lwb_y.data,...
 lwb_front = params.hardpoints_front.lwb.front.*ones(size(lwb_out));
 lwb_rear = params.hardpoints_front.lwb.rear.*ones(size(lwb_out));
 
-% calculate UWB and LWB plane normals
 uwb_n = zeros(size(uwb_out));
 lwb_n = zeros(size(lwb_out));
 IA_P = zeros(size(lwb_out));
@@ -77,8 +86,8 @@ IA_N = zeros(size(lwb_out));
 FVIC = zeros(size(lwb_out));
 SVIC = zeros(size(lwb_out));
 RCH_heave = zeros(1,n_data);
-for i = 1:n_data
-    %Calculate normals
+for i = 1:n_data %Calculate heave properties
+    %Calculate UWB and LWB plane normals
     uwb_n(i,:) = cross(uwb_out(i,:) - uwb_front(i,:), uwb_out(i,:) - uwb_rear(i,:));
     lwb_n(i,:) = cross(lwb_out(i,:) - lwb_front(i,:), lwb_out(i,:) - lwb_rear(i,:));
     
@@ -97,35 +106,41 @@ end
 
 simOut.channels.FVIC = FVIC;
 simOut.channels.SVIC = SVIC;
-simOut.channels.l_RCH_heave = RCH_heave;
+simOut.channels.l_RCH_heave = RCH_heave';
 
 %% Metrics
 % Calculates several useful parameters for at-a-glance sim result analysis
 disp("Calculating simulation metrics...")
 metrics = struct();
 
-% metrics.CamberGain_heave_coef
+
+[val, idx] = min(abs(simOut.channels.z_contact_patch(vert_test_start:end))); 
+static_idx = vert_test_start + idx - 1; %Array index where sim result is closest to static position
+
+metrics.CamberGain_heave_coef = polyfit(simOut.channels.z_contact_patch(vert_test_start:end),simOut.channels.a_camber(vert_test_start:end),2);
 % metrics.CamberGain_roll_coef
-% metrics.CamberGain_steer_coef
-% metrics.BumpSteer_coef
+metrics.CamberGain_steer_coe = polyfit(simOut.channels.a_toe(1:steer_test_end),simOut.channels.a_camber(1:steer_test_end),2);
+metrics.BumpSteer_coef = polyfit(simOut.channels.z_contact_patch(vert_test_start:end),simOut.channels.a_toe(vert_test_start:end),2);
 % metrics.Ackermann_coef
-% metrics.MotionRatio_coef
-metrics.BumpTravel = max(channels.z_contact_patch);
-metrics.DroopTravel = abs(min(channels.z_contact_patch));
-% metrics.RCH_static
-% metrics.RCH_heave_coef
+metrics.MotionRatio_coef = polyfit(simOut.channels.z_contact_patch(vert_test_start:end),simOut.channels.l_damper(vert_test_start:end),2);
+metrics.BumpTravel = max(simOut.channels.z_contact_patch);
+metrics.DroopTravel = abs(min(simOut.channels.z_contact_patch));
+metrics.RCH_static = simOut.channels.l_RCH_heave(static_idx);
+metrics.RCH_heave_coef = polyfit(simOut.channels.z_contact_patch(vert_test_start:end),simOut.channels.l_RCH_heave(vert_test_start:end),2);
 % metrics.RCH_roll_vert_coef
 % metrics.RCH_roll_lat_coef
 % metrics.AntiDive_static
 % metrics.AntiDive_coef
-% metrics.SteerJacking
-% metrics.KPI_static
-% metrics.Caster_static
-% metrics.ScrubRadius_static
-% metrics.MechanicalTrail_static
-% metrics.MaxSteer_inside
-% metrics.MaxSteer_outside
-
+metrics.SteerJacking_coef = polyfit(simOut.channels.a_toe(1:steer_test_end),simOut.channels.z_contact_patch(1:steer_test_end),2);
+metrics.KPI_static = simOut.channels.a_KPI(static_idx);
+metrics.Caster_static = simOut.channels.a_caster(static_idx);
+metrics.ScrubRadius_static = simOut.channels.l_scrubRad(static_idx);
+metrics.MechanicalTrail_static = simOut.channels.l_mechTrail(static_idx);
+metrics.l_damper_static = simOut.channels.l_damper(static_idx);
+%Assuming that largest steering angles are found in steer test - might not 
+%be true with crazy bump steer but at that point you have bigger issues...
+metrics.MaxSteer_inside = max(simOut.channels.a_toe); 
+metrics.MaxSteer_outside = min(simOut.channels.a_toe);
 
 simOut.metrics = metrics;
 
