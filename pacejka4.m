93815bfbf39a7c64438e17c1a7b8c185a560b626
Will <william.foster14@imperial.ac.uk>
2019-01-29 01:18:44 +0000

    Latest

---
+++ pacejka4.m
@@ -0,0 +1,14 @@
+function [force] = pacejka4(coef, slip)
+%PACEJKA4 Summary of this function goes here
+%   Detailed explanation goes here
+B = coef(1);
+C = coef(2);
+D = coef(3);
+E = coef(4);
+Sv = coef(5); %Vertical shift
+Sh = coef(6); %Horizontal shift
+
+x = slip + Sh;
+y = D*sin(C*atan(B*x - E*(B*x - atan(B*x))));
+force = y + Sv;
+end
