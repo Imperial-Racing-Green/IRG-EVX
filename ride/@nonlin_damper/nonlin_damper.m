ac1c16c86700e75211e0804f42c2647025189640
Owen Heaney <oheaney380@gmail.com>
2019-04-07 16:04:37 +0100

    Ride model progress
    Still not useable - chassis moves the wrong way with vertical base displacement,; shock absorber also goes wrong way

--- ride/@nonlin_damper/nonlin_damper.m
+++ ride/@nonlin_damper/nonlin_damper.m
@@ -11,13 +11,23 @@ classdef nonlin_damper
     end
     
     methods
-        function obj = nonlin_damper(spec,settings_compression,settings_rebound)
+        function obj = nonlin_damper(spec,settings_compression,...
+                settings_rebound,LS_adjust,HS_adjust)
             %NONLIN_DAMPER Construct an instance of this class
             %   Detailed explanation goes here
             obj.damper_spec = spec;
             obj.valve_settings_compression = settings_compression;
             obj.valve_settings_rebound = settings_rebound;
+            obj.adjuster_settings_low = LS_adjust;
+            obj.adjuster_settings_high = HS_adjust;
             
+            for i = 1:length(settings_compression)
+                for j = 1:length(settings_rebound)
+                    for k = 1:length(LS_adjust)
+                        
+                    end
+                end
+            end
         end
         
         function outputArg = method1(obj,inputArg)
@@ -26,7 +36,7 @@ classdef nonlin_damper
             outputArg = obj.Property1 + inputArg;
         end
         
-        obj = fit_setting(obj,setting_high, setting_low)
+        obj = fit_setting(obj,setting_high, setting_low, setting_high, setting_low)
     end
 end
 
