c4a159c87a78a169b5874fc5a74548122bb6ccdd
GregJones96 <45271807+GregJones96@users.noreply.github.com>
2020-02-17 13:38:08 +0000

    Reducing number of datapoints in acceleration and skidpad track to speed up simulation time. Delete outdated versions of these trackmaps

--- SkidPad_Track_Creator.m
+++ SkidPad_Track_Creator.m
@@ -1,20 +1,20 @@
 clear
 clc
 
+NPoints = 100;
 R = 9.13;
+
 centre = [-R 0];
-th = 0:pi/300:2*pi;
+th = linspace(0,2*pi,NPoints);
 x = [R * cos(th) + centre(1)]';
 y = [R * sin(th) + centre(2)]';
 plot(x,y);
 axis equal
 
-Spacing = ((diff(x)).^2 + (diff(y)).^2).^0.5;
-dist = cumsum(Spacing);  
-distanceTrack = [ 0 ; dist ; 2*pi*R];
+distanceTrack = linspace(0,2*pi*R,NPoints);
 radius_d = [R*(ones(1,length(x)))]';
 curve_d = 1./radius_d;
-theta_d = [0 ; cumsum(diff(distanceTrack).*curve_d)];
+theta_d = th;
 
-% save('SkidPad_Track.mat','x','y','radius_d','curve_d','theta_d','distanceTrack')
+save('SkidPad_Track.mat','x','y','radius_d','curve_d','theta_d','distanceTrack')
 
